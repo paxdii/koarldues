784ac79cd73952320212d121ccd17d1dc5868214
paxdii <adi.saff@gmail.com>
2023-03-06 11:16:36 +0100

    javascript-function

--- src/gallery.js
+++ src/gallery.js
@@ -1,7 +1,37 @@
 import PhotoSwipeLightbox from 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.6/photoswipe-lightbox.esm.min.js';
+import images from './photos.js';
+
 const lightbox = new PhotoSwipeLightbox({
   gallery: '#gallery--getting-started',
   children: 'a',
   pswpModule: () => import('https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.6/photoswipe.esm.min.js')
 });
 lightbox.init();
+
+
+
+function generateGallery() {
+  const gallery = document.getElementById("gallery");
+
+  for (const image of images) {
+    const aElement = document.createElement("a");
+    aElement.href = image.url;
+    aElement.setAttribute("data-pswp-width", image.width);
+    aElement.setAttribute("data-pswp-height", image.height);
+    aElement.target = "_blank";
+
+    const imgElement = document.createElement("img");
+    imgElement.src = image.url;
+    imgElement.alt = image.caption;
+    imgElement.classList.add("img-fluid");
+
+    aElement.appendChild(imgElement);
+
+    const colElement = document.createElement("div");
+    colElement.classList.add("col-2");
+    colElement.appendChild(aElement);
+
+    gallery.appendChild(colElement);
+  }
+}
+ export default generateGallery();
---
+++ src/photos.js
@@ -0,0 +1,17 @@
+const images = [
+    {
+      url: "../images/pic1.jpg",
+      caption: "Image 1",
+      width: 200,
+      height: 200
+    },
+    {
+      url: "../images/pic2.jpg",
+      caption: "Image 2"
+    },
+    {
+      url: "../images/pic3.png",
+      caption: "Image 3"
+    }
+  ];
+  export default images;
